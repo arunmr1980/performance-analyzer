9ab23162237a8ae63897ade8b5b148dbe38acfc8
Arun MR <arun.raveendranathan@cleareye.ai>
2024-01-26 12:28:09 +0530

    JSON file view

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -3,15 +3,14 @@ import streamlit as st
 left_column, right_column = st.columns(2)
 
 with left_column:
-    xml_file = st.file_uploader("Upload the MT7XX XML file")
+    json_file = st.file_uploader("Upload the performance JSON")
 
 if xml_file is not None:
-    xml_string = xml_file.read()
+    json_string = json_file.read()
     print("Reading xml file")
-    print(xml_string)
 
     with left_column:
-        st.write(xml_string)
+        st.json(json_string)
     mt7xx_txt = ""
     with right_column:
         st.write("MT7XX text file")
